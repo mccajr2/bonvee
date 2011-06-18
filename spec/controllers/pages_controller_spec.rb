a84f634544f321636b7accf14d3e5bc6f25140eb
Jason McCarthy <mccarthy.jay@gmail.com>
2011-06-17 20:55:09 -0400

    Changes from static page exercises

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Bonvee.com"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,7 +16,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Bonvee.com | Home")
+                        :content => @base_title + " | Home")
     end
   end
 
@@ -26,7 +30,7 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "Bonvee.com | Contact")
+                          @base_title + " | Contact")
     end
   end
 
@@ -40,7 +44,21 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Bonvee.com | About")
+                          @base_title + " | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          "Bonvee.com | Help")
     end
   end
 end
