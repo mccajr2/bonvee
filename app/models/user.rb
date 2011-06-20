80a1a5f49c82820040f28cd7fbdffd486daa2e10
Jason McCarthy <mccarthy.jay@gmail.com>
2011-06-20 16:25:31 -0400

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@
 require 'digest'
 class User < ActiveRecord::Base
   attr_accessor :password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :location, :tagline
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
