3df5f40c1844e8433c8f6bbfefbba7dc855d4d00
Jason McCarthy <mccarthy.jay@gmail.com>
2011-06-20 22:57:43 -0400

    Quitting for the night before paginate tests

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'gravatar_image_tag', '1.0.0.pre2'
 group :development do
   gem 'rspec-rails', '2.6.1'
   gem 'annotate-models', '1.0.4'
+  gem 'faker', '0.3.1'
+  gem 'will_paginate', '3.0.pre2'
 end
 
 group :test do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+  
   def new
     @user = User.new
     @title = "Sign up"
+  end
+  
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
   end
   
   def show
@@ -20,5 +28,32 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    #@user = User.find(params[:id])
+    @title = "Edit user"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
